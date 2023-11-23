3cbaf37a6ecfb401b9e75f3e7dda8e8ae1311b78
Mohan raj <147843991+Mohanguvi@users.noreply.github.com>
2023-11-24 00:33:13 +0530

    First_Non_repeating_element.py

--- Non_repeating.py
+++ Non_repeating.py
@@ -1,11 +1,12 @@
-# Given list of integers
-Given_list = [int(x) for x in input("Enter the list of number : ").split()]
+# Get the user input value - list of integers
+M = input("Enter the element by spaces: ")
+list = M.split()
 
 # Create a dictionary to store the frequency of each element
 n = {}
 
 # Count the frequency of each element in the list
-for num in Given_list:
+for num in list:
     if num in n:
         n[num] += 1
     else:
@@ -13,7 +14,7 @@ for num in Given_list:
 
 # Iterate through the list again to find the first non-repeating element
 first_non_repeat = None
-for num in Given_list:
+for num in list:
     if n[num] == 1:
         first_non_repeating = num
         break
